29ca3964cffdff3714e46d6622b05b5a25471f85
Yash <maddysai9848@gmail.com>
2025-01-14 00:06:31 +0530

    trail3

--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Layout from './Layout.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home.jsx'
 import AboutPage from './components/AboutPage.jsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
-    element: <Layout/>,
-    children:[
+    element: <Layout />,
+    children: [
       {
-        path : '',
-        element : <Home />
+        path: '',
+        element: <Home />,
       },
       {
-        path : '/about',
-        element : <AboutPage />
-      }
-    ]
-  }
-])
+        path: '/about',
+        element: <AboutPage />,
+      },
+    ],
+  },
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/technoit-v2-yash"
+  base : "/technoit-v2-yash/"
 })
